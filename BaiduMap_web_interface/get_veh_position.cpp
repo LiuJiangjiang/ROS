a439b0bdb70cb482a035e1190f31a480e7908be3
LiuJiangjiang <jiangjiangliu.ustb@hotmail.com>
2018-06-19 19:48:13 +0800

    Add files via upload
    get the map picture based on the given GPS, and display it in ROS.

---
+++ BaiduMap_web_interface/get_veh_position.cpp
@@ -0,0 +1,67 @@
+/**********************************************************
+*	FileName: get_veh_position.cpp
+*	Author	: LJJ
+*	Date	: 2018.06.12
+*	Describe: ...
+**********************************************************/
+#include <ros/ros.h>
+#include <hirain_visualization/Ego.h>
+
+ros::Publisher veh_position_pub;
+
+hirain_visualization::Ego msg;
+
+int count = 0;
+
+void get_veh_position()
+{
+	double lat[] = {40.1007, 20.1023, 30.1027, 40.1031, 30.1035};
+	double lon[] = {136.287, 106.286, 126.286, 116.285, 110.285};
+
+
+	if(count >=0 && count <=2000)
+	{
+		msg.lat = lat[0];
+		msg.lon = lon[0];
+	}
+	else if(count > 2000 && count <= 4000)
+	{
+		msg.lat = lat[1];
+		msg.lon = lon[1];
+	}
+	else if(count > 4000 && count <= 6000)
+	{
+		msg.lat = lat[2];
+		msg.lon = lon[2];
+	}
+	else if(count > 6000 && count <=8000)
+	{
+		msg.lat = lat[3];
+		msg.lon = lon[3];
+	}
+	else if(count > 8000 && count <= 10000)
+	{
+		msg.lat = lat[4];
+		msg.lon = lon[4];
+	}
+	else
+		count = 0;
+	veh_position_pub.publish(msg);
+	count++;
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "get_veh_position");
+	ros::NodeHandle n;
+	veh_position_pub 	= n.advertise<hirain_visualization::Ego>("veh_positon", 1);
+	ros::Rate loop_rate(10);
+
+	while(1)
+	{
+		get_veh_position();
+		ros::spinOnce();
+		loop_rate.sleep();
+	}	
+	return 0;	
+}
---
+++ BaiduMap_web_interface/veh_web_map_disp.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+
+from __future__ import print_function
+import urllib
+
+import rospy
+import roslib
+roslib.load_manifest("hirain_visualization")
+import sys
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+from hirain_visualization.msg import Ego
+
+ss = 'http://api.map.baidu.com/staticimage/v2?ak=hK1vUDiX7NVayGcV3SdK1QIY38bcfPhW&mcode=666666&center'
+picName = '/home/hirain/catkin_ws/src/hirain_visualization/pics/veh_loaction.png'
+
+def veh_web_map_callback(data):
+	lon = data.lon
+	lat = data.lat
+	print (lat)
+	url = ss + str(lon) + ',' + str(lat) + '&width=300&height=200&zoom=15&scale=2&markers=' + str(lon) + ',' + str(lat) + '&markerStyles=l'
+	urllib.urlretrieve(url,picName)
+	web_map_pub = rospy.Publisher("disp_veh_positon_map", Image)
+	img = cv2.imread(picName)
+	web_map_pub.publish(CvBridge().cv2_to_imgmsg(img, "bgr8"))
+
+def listener():
+	rospy.init_node('veh_web_map_disp', anonymous = True)
+	rospy.Subscriber("veh_positon", Ego, veh_web_map_callback)
+	rospy.spin()
+
+if __name__=='__main__':
+	listener()
+
+#url = 'http://api.map.baidu.com/staticimage/v2?ak=hK1vUDiX7NVayGcV3SdK1QIY38bcfPhW&mcode=666666&center116.403874,39.914888&width=300&height=200&zoom=15&scale=2&markers=116.403874,39.9148881&markerStyles=l'
+
+##################### Methond 1 #####################
+#picName = "hello.png"
+#chrome_options = Options()
+#chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--disable-gpu')
+#driver = webdriver.Chrome(chrome_options=chrome_options)
+#driver.get(url)
+#driver.maximize_window()
+#driver.save_screenshot(picName)
+#driver.close()
+
+##################### Methond 2 #####################
+#urllib.urlretrieve(url,'test.png')
